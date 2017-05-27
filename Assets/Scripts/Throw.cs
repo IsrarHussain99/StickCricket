5341dfd107b264d877bc1be3ac78eca2b99e0ce0
Javvaji <javvajikiran167@gmail.com>
2017-05-27 19:04:33 +0530

    second commit

---
+++ Assets/Scripts/Throw.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Throw : MonoBehaviour {
+
+	public GameObject ball_prefab;
+	public static bool ready_to_throw_ball ;
+
+	// Use this for initialization
+	void Start () {
+		ready_to_throw_ball = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//if (ready_to_throw_ball)
+			
+		if (ready_to_throw_ball) {
+			ThrowBall ();
+		}
+	}
+
+	void ThrowBall(){
+		GameObject ball_clone = (GameObject) Instantiate(ball_prefab, ball_prefab.transform.position, ball_prefab.transform.rotation);
+		Rigidbody ball_rb = ball_clone.GetComponent<Rigidbody>();
+		float horizontal_angle = Mathf.Atan (8.0f / 11.0f);
+		float vertical_angle = 1.047f; //60 degrees
+
+		float velocity = 15.0f;
+		float horizontal_force = 15.0f;
+
+
+		ball_rb.velocity.Set (-velocity*Mathf.Cos(vertical_angle)*Mathf.Sin(horizontal_angle),velocity*Mathf.Sin(vertical_angle),-velocity*Mathf.Cos(vertical_angle)*Mathf.Cos(horizontal_force));
+		ball_rb.AddForce (-horizontal_force*Mathf.Sin(horizontal_angle),0,-horizontal_force*Mathf.Cos(horizontal_angle),ForceMode.Impulse);
+		ready_to_throw_ball = false;
+
+
+		//after hit by bat destroy it
+	}
+
+
+}
---
+++ Assets/Scripts/hit.cs
@@ -0,0 +1,123 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hit : MonoBehaviour {
+
+	public float thrust;
+	// Use this for initialization
+	void Start () {
+
+		InvokeRepeating("NextChance",5f);
+			
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		/*if ( (Input.GetKeyDown(KeyCode.LeftArrow) && Input.GetKey(KeyCode.UpArrow)) || (Input.GetKeyDown(KeyCode.UpArrow) && Input.GetKey(KeyCode.LeftArrow)))
+		{
+			Debug.Log("LU");
+		}
+		else if ( (Input.GetKeyDown(KeyCode.UpArrow) && Input.GetKey(KeyCode.RightArrow)) || (Input.GetKeyDown(KeyCode.RightArrow) && Input.GetKey(KeyCode.UpArrow)))
+		{
+			Debug.Log("RU");
+
+		}
+		else if ( (Input.GetKeyDown(KeyCode.RightArrow) && Input.GetKey(KeyCode.DownArrow)) || (Input.GetKeyDown(KeyCode.DownArrow) && Input.GetKey(KeyCode.RightArrow)))
+		{
+			Debug.Log("RD");
+		}
+		else if ( (Input.GetKeyDown(KeyCode.LeftArrow) && Input.GetKey(KeyCode.DownArrow)) || (Input.GetKeyDown(KeyCode.DownArrow) && Input.GetKey(KeyCode.LeftArrow)))
+		{
+			Debug.Log("LD");
+		}
+		else if (Input.GetButtonDown("Fire1"))
+        {
+					Debug.Log("Up");
+        }*/
+		if (Throw.ready_to_throw_ball)
+		{
+		if (Input.GetKeyDown (KeyCode.DownArrow)) {
+			Debug.Log ("Down");
+			transform.Rotate (0, -45, 0);
+			Rigidbody rb = GetComponent<Rigidbody> ();
+			//rb.velocity.Set ();
+			rb.isKinematic = true;
+			rb.AddForce (transform.right * thrust);
+					
+		} else if (Input.GetKeyDown (KeyCode.LeftArrow)) {
+			Debug.Log ("Left");
+			transform.Rotate (45, 0, 0);
+			Rigidbody rb = GetComponent<Rigidbody> ();
+			//rb.velocity.Set ();
+			rb.isKinematic = true;
+			rb.AddForce (transform.right * thrust);
+
+		} else if (Input.GetKeyDown (KeyCode.UpArrow)) {
+			transform.Rotate (0, 45, 0);
+			Debug.Log ("Up");
+			Rigidbody rb = GetComponent<Rigidbody> ();
+			//rb.velocity.Set ();
+			rb.isKinematic = true;
+			rb.AddForce (transform.right * thrust);
+
+		} else if (Input.GetKeyDown (KeyCode.RightArrow)) {
+			Debug.Log ("Right");
+			transform.Rotate (-45, 0, 0);
+			Rigidbody rb = GetComponent<Rigidbody> ();
+			//rb.velocity.Set ();
+			rb.isKinematic = true;
+			rb.AddForce (transform.right * thrust);
+
+		} else if (Input.GetKeyDown (KeyCode.Q)) {
+			Debug.Log ("Q");
+			transform.Rotate (45, 45, 0);
+			Rigidbody rb = GetComponent<Rigidbody> ();
+			//rb.velocity.Set ();
+			rb.isKinematic = true;
+			rb.AddForce (transform.right * thrust);
+
+		} else if (Input.GetKeyDown (KeyCode.W)) {
+			Debug.Log ("W");
+			transform.Rotate (-45, 45, 0);
+			Rigidbody rb = GetComponent<Rigidbody> ();
+			//rb.velocity.Set ();
+			rb.isKinematic = true;
+			rb.AddForce (transform.right * thrust);
+
+		} else if (Input.GetKeyDown (KeyCode.A)) {
+			Debug.Log ("A");
+			transform.Rotate (45, -45, 0);
+			Rigidbody rb = GetComponent<Rigidbody> ();
+			rb.isKinematic = true;
+			rb.AddForce (transform.right * thrust);
+		} else if (Input.GetKeyDown (KeyCode.S)) {
+			Debug.Log ("S");
+			transform.Rotate (-45, -45, 0);
+			Rigidbody rb = GetComponent<Rigidbody> ();
+			//rb.velocity.Set ();
+			rb.isKinematic = true;
+			rb.AddForce (transform.right * thrust);
+
+		}
+		//Wait ();
+	}
+	}
+
+    
+
+	IEnumerator Wait()
+	{
+		yield return new WaitForSeconds(10);
+
+	}
+
+	void NextChance()
+	{
+		transform.rotation.Set (-90, 0, 90, 0);
+		Rigidbody rb = GetComponent<Rigidbody>();
+		rb.isKinematic = false;
+		Throw.ready_to_throw_ball = true;
+
+	}
+}
